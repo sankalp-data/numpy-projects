4e168036be1aff49119d58a49f5311a878b49b81
sankalp-data <sankalp.ds01@gmail.com>
2025-08-11 22:03:50 +0530

    Started working on Cryptocurrency-Pump-Dump-Detector

---
+++ Intermediate-Projects/Cryptocurrency-Pump-Dump-Detector/crypto_dump_pump_detector.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+with open(r"C:\Users\usar\Downloads\btc_hourly_ohlcv.csv",encoding="utf-8") as f:
+    headers = f.readline().strip().split(",")
+
+
+
+data = np.genfromtxt(r"C:\Users\usar\Downloads\btc_hourly_ohlcv.csv",delimiter=",",dtype=None,encoding="utf-8",skip_header=1)
+# print(data)
+# print(data[0])
+
+def volume_spike_detector():
+    '''Calculate average volume over past week.'''
+
+    
+
+
+
+
+
+
+
+
